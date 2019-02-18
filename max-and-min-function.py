b3db205993eb178e88d6c165f244b1173227e6ef
bdclauser <pzljug@hotmail.com>
2019-02-17 21:11:18 -0500

    min and max example update

---
+++ max-and-min-function.py
@@ -0,0 +1,9 @@
+# Takes any number of arguments and returns the largest one.
+maximum = max(-5, -10, 5, 10, 20, 30, -500)
+
+print(maximum)
+
+# Takes an number of arguments and returns the smallest one.
+minimum = min(-5, -10, 5, 10, 20, 30, -500)
+
+print(minimum)
--- max-function.py
+++
@@ -1,4 +0,0 @@
-# Takes any number of arguments and returns the largest one.
-maximum = max(-5, -10, 5, 10, 20, 30, -500)
-
-print(maximum)
